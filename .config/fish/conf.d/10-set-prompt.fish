4900b75706f76cf0ccd8fa9aad94be95c8d4e069
Juha Ruotsalainen <kontza@gmail.com>
2024-09-02 15:38:10 +0300

    Git prompt refactored

--- .config/fish/conf.d/10-set-prompt.fish
+++ .config/fish/conf.d/10-set-prompt.fish
@@ -1,37 +1,33 @@
-set -g __fish_git_prompt_show_informative_status 1
-set -g __fish_git_prompt_hide_untrackedfiles 1
-
-set -g __fish_git_prompt_color_branch magenta
-set -g __fish_git_prompt_showupstream informative
-set -g __fish_git_prompt_char_upstream_ahead "↑"
-set -g __fish_git_prompt_char_upstream_behind "↓"
-set -g __fish_git_prompt_char_upstream_prefix ""
-
-set -g __fish_git_prompt_char_stagedstate "●"
-set -g __fish_git_prompt_char_dirtystate "✚"
-set -g __fish_git_prompt_char_untrackedfiles "…"
-set -g __fish_git_prompt_char_conflictedstate "✖"
-set -g __fish_git_prompt_char_cleanstate "✔"
-
-set -g __fish_git_prompt_color_dirtystate blue
-set -g __fish_git_prompt_color_stagedstate yellow
-set -g __fish_git_prompt_color_invalidstate red
-set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-set -g __fish_git_prompt_color_cleanstate green
-
 function fish_prompt
-    set VTAG ""
+    set -g __fish_git_prompt_show_informative_status 1
+    set -g __fish_git_prompt_showcolorhints 1
+    set -g __fish_git_prompt_showdirtystate 1
+    set -g __fish_git_prompt_showuntrackedfiles 0
+    set -g __fish_git_prompt_describe_style branch
+
+    set -g __fish_git_prompt_showupstream informative
+    set -g __fish_git_prompt_char_upstream_ahead "↑"
+    set -g __fish_git_prompt_char_upstream_behind "↓"
+    set -g __fish_git_prompt_char_upstream_prefix ""
+
+    set -g __fish_git_prompt_char_stagedstate "●"
+    set -g __fish_git_prompt_char_dirtystate "✚"
+    set -g __fish_git_prompt_char_untrackedfiles "…"
+    set -g __fish_git_prompt_char_conflictedstate "✖"
+    set -g __fish_git_prompt_char_cleanstate "✔"
+
+    printf "%s:" (prompt_login)
+    set_color bryellow
+    echo -n (prompt_pwd -d1 -D2)
+    set_color normal
+    echo -n (fish_git_prompt)
     if test -d .git
         set MASTER_NAME (git branch -r|rg origin/ma|tail -1|tr '/' '\n'|tail -1)
         set SYSTEM_PROMPT (fish_git_prompt)
         string match -eq $MASTER_NAME $SYSTEM_PROMPT
         if test $status -eq 0
-            set VTAG (git describe --tags 2> /dev/null)
+            echo -n '' (git describe --tags 2> /dev/null)
         end
     end
-    printf "%s:%s%s %s\n❯ " \
-        (prompt_login) \
-        (prompt_pwd -d1 -D2) \
-        (fish_git_prompt) \
-        $VTAG
+    echo -e "\n❯ "
 end
