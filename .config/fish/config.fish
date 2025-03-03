f815ed0c0192f4c08afcbaae4e3fb20f969e143b
Juha Ruotsalainen <juha.ruotsalainen@iki.fi>
2025-03-03 20:59:14 +0200

    New bindings as per tips on https://github.com/fish-shell/fish-shell/issues/11206

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,9 @@
+bind escape,backspace backward-kill-word
+bind escape,d kill-word
+bind escape,f forward-word
+bind escape,b backward-word
 set -x fish_escape_delay_ms 200
 set -g theme_short_path yes
 # Disable greeting.
-function fish_greeting;end
-
+function fish_greeting
+end
